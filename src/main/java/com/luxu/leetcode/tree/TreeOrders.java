94efbbfbe893da04bead0f836146f16899e23c5b
empty117 <xu.1.lu@nokia.com>
2017-09-07 10:17:54 +0800

    Binary tree oprations

--- src/main/java/com/luxu/leetcode/tree/TreeOrders.java
+++ src/main/java/com/luxu/leetcode/tree/TreeOrders.java
@@ -2,16 +2,16 @@ package com.luxu.leetcode.tree;
 
 import com.luxu.leetcode.util.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by xulu on 2017/8/23.
  */
 public class TreeOrders {
-    int sum = 0;
-    public int findTilt(TreeNode root) {
-        postOrder(root);
-        return sum;
-    }
-
+    /*
+    中左右
+     */
     public void preOrder(TreeNode t){
         if(t!=null){
             System.out.print(t.val+" ");
@@ -19,15 +19,101 @@ public class TreeOrders {
             preOrder(t.right);
         }
     }
-    public int postOrder(TreeNode root){
-        if(root == null){
+
+    public void preOrderNoRecursion(TreeNode t){
+        LinkedList<TreeNode> stack = new LinkedList();
+        while(t!=null || !stack.isEmpty()){
+            while(t!=null){
+                System.out.print(t.val + " ");
+                stack.push(t);
+                t = t.left;
+            }
+            if(!stack.isEmpty()){
+                t = stack.pop();
+                t = t.right;
+            }
+        }
+    }
+
+    /*
+    左右中
+     */
+    public void postOrder(TreeNode t){
+        if(t!=null){
+            postOrder(t.left);
+            postOrder(t.right);
+            System.out.print(t.val+" ");
+        }
+    }
+
+    public void postOrderNoRecursion(TreeNode t){
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<TreeNode> temp = new LinkedList<>();
+        while(t!=null || !stack.isEmpty()){
+            while(t!=null){
+                stack.push(t);
+                temp.push(t);
+                t = t.right;
+            }
+            if(!stack.isEmpty()){
+                t = stack.pop();
+                t = t.left;
+            }
+        }
+        temp.stream().forEach(item -> System.out.print(item.val+" "));
+    }
+
+
+    /*
+    左中右
+     */
+    public void midOrder(TreeNode t){
+        if(t!=null){
+            midOrder(t.left);
+            System.out.print(t.val+" ");
+            midOrder(t.right);
+        }
+    }
+
+    public void midOrderNoRecursion(TreeNode t){
+        LinkedList<TreeNode> stack = new LinkedList();
+        while(t!=null || !stack.isEmpty()){
+            while(t!=null){
+                stack.push(t);
+                t = t.left;
+            }
+            if(!stack.isEmpty()){
+                t = stack.pop();
+                System.out.print(t.val + " ");
+                t = t.right;
+            }
+        }
+    }
+
+    public void levelOrderNoRecursion(TreeNode t){
+        Queue<TreeNode> queue = new LinkedList();
+        if(t!=null){
+            queue.add(t);
+        }
+        while (!queue.isEmpty()){
+            t = queue.poll();
+            if(t!=null){
+                System.out.print(t.val+ " ");
+                queue.offer(t.left);
+                queue.offer(t.right);
+            }
+        }
+    }
+
+    public int getLevel(TreeNode t){
+        if(t==null){
             return 0;
         }
-        int leftSum = postOrder(root.left);
-        int rightSum = postOrder(root.right);
-        sum += Math.abs(leftSum - rightSum);
-        return root.val + leftSum + rightSum;
+        int l = getLevel(t.left);
+        int r = getLevel(t.right);
+        return l>r?l+1:r+1;
     }
+
     public static void main(String args[]){
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
@@ -39,6 +125,20 @@ public class TreeOrders {
 
         TreeOrders treeOrders = new TreeOrders();
         treeOrders.preOrder(root);
+        System.out.println();
+        treeOrders.postOrder(root);
+        System.out.println();
+        treeOrders.midOrder(root);
+        System.out.println();
+        treeOrders.preOrderNoRecursion(root);
+        System.out.println();
+        treeOrders.postOrderNoRecursion(root);
+        System.out.println();
+        treeOrders.midOrderNoRecursion(root);
+        System.out.println();
+        treeOrders.levelOrderNoRecursion(root);
+        System.out.println();
+        System.out.println(treeOrders.getLevel(root));
 //        int leftSum = treeOrders.title(root);
 //        System.out.println(treeOrders.findTilt(root));
     }
